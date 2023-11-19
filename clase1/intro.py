8f0bc975118cb1039c0bfe1f74afffb50e5a1c3c
maromo16 <mariaalejandraromoroman16@gmail.com>
2023-11-19 13:07:49 -0500

    subiendo archivos de clase introductoria

---
+++ clase1/intro.py
@@ -0,0 +1,19 @@
+print("Hola mundo")
+
+fecha = input("ingrese su fecha de nacimiento: ")
+print(fecha)
+print("la fecha ingresada es: "+fecha)
+num1 = 5
+num2 = 7.2
+nombre = "Male"
+print(num1 + num2)
+print(nombre)
+print("operaciones con print cadena")
+print(5/2)
+print(5//2)
+print(5%2)
+print(5.2/2.6)
+print(3*2)
+print(3**2)
+print(5<2)
+print(5>2)
